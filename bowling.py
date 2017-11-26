604180f288e3f3f4731b31f857f929b3f50d7a10
Peter Barazutti <peter@peter.barazutti>
2017-11-26 12:35:33 +0100

    Simplified and/or corrected some 'if' conditions

--- bowling.py
+++ bowling.py
@@ -10,7 +10,7 @@ def score(game):
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i].lower() == 'x':
                 result += get_value(game[i + 1])
                 if game[i + 2] == '/':
                     result += 10 - get_value(game[i + 1])
@@ -19,20 +19,18 @@ def score(game):
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[i].lower() == 'x':
             in_first_half = True
             frame += 1
     return result
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in [str(i) for i in range(1, 10)]:
         return int(char)
     elif char == 'X' or char == 'x':
         return 10
